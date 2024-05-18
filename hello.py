972aae48cce9148e76ca80ffb4d8b26ca93681df
Poulami-Das <poulami322@gmail.com>
2024-05-18 16:08:45 +0530

    hotfix is use for solving issues

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-def factorial(x):
+def fact(x):
 	if x==0:
 		return 1
 	else:
 		return x*factorial(x-1)
 number=s
-result=factorial(number)
+result=fact(number)
 print(f"the factorial of {number} is {result}")
