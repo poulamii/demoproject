b5d4f437ec327c75b1b1990dcb0892c842680aeb
Poulami-Das <poulami322@gmail.com>
2024-05-18 16:46:50 +0530

    hotfix fix issues in main code

--- hello.py
+++ hello.py
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-def factorial(i):
+def fact(i):
 	if i==0:
-=======
-=======
->>>>>>> hotfix
-def fact(x):
-	if x==0:
->>>>>>> hotfix
 		return 1
 	else:
-<<<<<<< HEAD
 		return i*factorial(i-1)
-=======
-		return x*fact(i-x)
->>>>>>> hotfix
 number=s
 result=fact(number)
 print(f"the factorial of {number} is {result}")
