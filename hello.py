e90c010712f5abd211d1905c83f8d55e74295b4f
Poulami-Das <poulami322@gmail.com>
2024-05-18 15:36:32 +0530

    hotfix:fix the issues in main code

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-def factorial(x):
-	if x==0:
+def factorial(i):
+	if i==0:
 		return 1
 	else:
-		return x*factorial(x-1)
+		return i*factorial(i-1)
 number=s
 result=factorial(number)
 print(f"the factorial of {number} is {result}")
